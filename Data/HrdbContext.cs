254d5551cd23e6f8ef7f904f7cd45202f9a28978
Khin Su Mon <sumonkk@gmail.com>
2024-11-01 10:14:11 +0630

    Update HrdbContext.cs

--- Data/HrdbContext.cs
+++ Data/HrdbContext.cs
@@ -22,7 +22,7 @@ public partial class HrdbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server= LAPTOP-ILC2KDKS; database=HRDB;trusted_connection=True; TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("server= smkk03; database=HRDB;trusted_connection=True; TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
